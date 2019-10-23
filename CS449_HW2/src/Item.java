6619260c122066a19b4999d2cacca62e92edd85b
Alec Shern <alecshern@gmail.com>
2019-10-23 16:29:50 -0500

    Init commit

---
+++ CS449_HW2/src/Item.java
@@ -0,0 +1,23 @@
+
+public class Item {
+
+	private String title;
+	
+	private double originalUnitPrice;
+	
+	private int quantity;
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public double getItemTotal() {
+		return originalUnitPrice;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	
+}
